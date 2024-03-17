32722e280b513c5b65de7d02654c20ba6531f8e1
Jackson raupp <jraupp@c1r7p1.42sp.org.br>
2024-03-17 15:50:16 -0300

    resolvido leaks da implementação do union - Commit on 2024.03.17	15h50m16

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jraupp <jraupp@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 10:46:24 by jraupp            #+#    #+#             */
-/*   Updated: 2024/03/17 13:52:34 by jraupp           ###   ########.fr       */
+/*   Updated: 2024/03/17 15:26:18 by jraupp           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,7 +93,7 @@ void		free_list(t_list *list, void (f)(t_list *));
 void		free_tokens(t_list *tokens);
 void		free_env_list(t_list *env_list);
 
-//env_list.c
+// env_list.c
 t_list		*make_env_list(char **envp, t_list *env_list);
 char		*find_name(char *envp);
 char		*find_value(char *envp);
@@ -114,16 +114,16 @@ t_list		*process_redirect_output(t_list *tokens, char *input, int i);
 int			form_word(t_list *tokens, int signal, char *input, int i);
 int			find_len(char *input, int signal);
 
-//type assignment.C
+// type assignment.C
 t_list		*type_assignment(t_list *tokens);
 
-//commands.c
+// commands.c
 t_list		*is_command(t_list *tokens);
 int			ft_strlcmp(char *s1, char *s2, int len);
 int			compare_quoted_strings(char *s1, char *s2);
 t_list		*is_builtin(t_list *tokens);
 
-//files.c
+// files.c
 t_list		*is_file(t_list *tokens);
 t_list		*is_heredoc_key(t_list *tokens);
 
@@ -147,7 +147,7 @@ int			is_redirect_output(char chr);
 int			is_heredoc(char chr, char next_chr);
 int			is_append(char chr, char next_chr);
 
-//utils_tokens.c
+// utils_tokens.c
 void		print_tokens(t_node *token);
 
 #endif
--- source/lexer/tokenization.c
+++ source/lexer/tokenization.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenization.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlamark- <dlamark-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jraupp <jraupp@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 13:27:12 by jraupp            #+#    #+#             */
-/*   Updated: 2024/03/17 15:08:01 by dlamark-         ###   ########.fr       */
+/*   Updated: 2024/03/17 15:19:05 by jraupp           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,8 @@ int	process_delimiter(t_list *tokens, int signal, char *input, int i)
 		else if (is_pipe(input[i]))
 		{
 			tokens = add_node(tokens);
-			tokens->node->value = ft_strdup("|");
 			tokens->node->data = ft_calloc(1, sizeof(int));
+			tokens->node->value = ft_strdup("|");
 			tokens->node->data->type = PIPE;
 		}
 	}
