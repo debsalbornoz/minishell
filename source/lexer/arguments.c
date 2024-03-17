15fb489d9ba5de4e104307dc7014cd5b73696071
debora <debsalbornoz@gmail.com>
2024-03-17 15:39:52 -0300

    adicionei argumentos e concertei os tipos depois dos redirects

--- source/lexer/arguments.c
+++ source/lexer/arguments.c
@@ -1,23 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   type_assignment.c                                  :+:      :+:    :+:   */
+/*   arguments.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dlamark- <dlamark-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/12 21:26:00 by dlamark-          #+#    #+#             */
-/*   Updated: 2024/03/17 15:33:37 by dlamark-         ###   ########.fr       */
+/*   Created: 2024/03/17 15:25:59 by dlamark-          #+#    #+#             */
+/*   Updated: 2024/03/17 15:37:21 by dlamark-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-t_list	*type_assignment(t_list *tokens)
+t_list	*is_argument(t_list *tokens)
 {
-	tokens = is_command(tokens);
-	tokens = is_argument(tokens);
-	tokens = is_builtin(tokens);
-	tokens = is_file(tokens);
-	tokens = is_append_or_heredoc_key(tokens);
+	tokens->node = tokens->head;
+	if (tokens->node)
+	{
+		while (tokens->node != NULL)
+		{
+			if (tokens->node->data->type == WORD
+				&& tokens->node->data->type != COMMAND)
+				tokens->node->data->type = ARGUMENT;
+			tokens->node = tokens->node->next;
+		}
+	}
+	tokens->node = tokens->head;
 	return (tokens);
 }
