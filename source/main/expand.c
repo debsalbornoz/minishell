dcc946e79d584025077bed47d731334f0b788d09
Jackson Raupp <JacksonRaupp@icloud.com>
2024-03-23 12:09:55 -0300

    função de expansão em andamento - Commit on 2024.03.23	12h09m54

--- source/main/expand.c
+++ source/main/expand.c
@@ -3,29 +3,97 @@
 /*                                                        :::      ::::::::   */
 /*   expand.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jraupp <jraupp@student.42.fr>              +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 20:59:57 by jraupp            #+#    #+#             */
-/*   Updated: 2024/03/20 21:10:30 by jraupp           ###   ########.fr       */
+/*   Updated: 2024/03/22 18:52:24 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-char  *expand(t_list *lst_env, char *input)
-{
-	char	signal;
-	char	*temp;
-	char	*varible;
+char	*process_heredoc(char signal, char *input);
+char	*find_variable_name(t_list *lst_env, char *input, char *position);
+char	*concatenation_chr(char *dest, char src);
 
-	signal = 0;
+char  *find_varible(t_list *lst_env, char *input)
+{
+	char	*temp;
+	char	signal_quote;
+	int		*signal_heredoc;
+
 	temp = input;
+	signal_quote = 0;
+	signal_heredoc = 0;
 	while (*temp)
 	{
-		signal = process_quotes(signal, *temp);
-		if (*temp == '$' && !is_single_quote(signal) && *(temp + 1))
+		signal_quote = process_quotes(signal_quote, *temp);
+		if (!is_single_quote(signal_quote))
+		{
+			if (is_double_quote(signal_quote))
+				temp = find_variable_name(lst_env, input, temp);
+			else
+			{
+				temp = process_heredoc(signal_quote, temp);
+				temp = find_variable_name(lst_env, input, temp);
+			}
+		}
 		temp++;
 	}
 	temp = 0;
+	return (input);
+}
+
+char	*process_heredoc(char signal, char *input)
+{
+	if (*input == '<' && *(input + 1) == '<')
+	{
+		input++;
+		input++;
+		input = trim_start_spaces(input);
+		while (!signal && *(input + 1) && !is_space(*(input + 1)))
+			input++;
+	}
+	return (input);
+}
+
+char	*find_variable_name(t_list *lst_env, char *input, char *position)
+{
+	char	*temp;
+	char	*variable_name;
+
+	variable_name = 0;
+	temp = position;
+	if (*temp == '$')
+	{
+		temp++;
+		while (*temp && (ft_isalnum(*temp) || *temp == '_'))
+			variable_name = concatenation_chr(variable_name, *temp++);
+	}
+	if (variable_name)
+		free(variable_name);
+	return (input);
+}
+
+char	*concatenation_chr(char *dest, char src)
+{
+	char	*temp;
+
+	temp = 0;
+	if (!dest)
+	{
+		temp = ft_calloc(2, sizeof(char));
+		*temp = src;
+		return (temp);
+	}
+	temp = ft_calloc(1, ft_strlen(dest) + 2);
+	while(*dest)
+		*temp++ = *dest++;
+	*temp = src;
+	return (temp);
+}
+
+char	*expand(t_list *lst_env, char *name, char *input)
+{
 	return (input);
 }
