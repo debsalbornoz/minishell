d348c6b959fa59dab2452b799891b4497af17173
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2024-05-07 15:00:23 +0000

    organizando o projeto

--- source/main/program.c
+++ source/main/program.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 13:46:23 by jraupp            #+#    #+#             */
-/*   Updated: 2024/05/06 13:55:50 by codespace        ###   ########.fr       */
+/*   Updated: 2024/05/07 14:59:32 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,7 @@ int	program(t_list *envp)
 	if (!*input)
 		return (TRUE);
 	input = expand(envp, input);
-	if (!is_closed(input))
-		return (FALSE);
 	tokens = *lexer(&tokens, input);
-	tokens = *type_assignment(&tokens);
 	if (!parsing(&tokens, envp, input))
 		return (TRUE);
 	exec = *execution(&tokens, &exec, envp);
--- source/parser/parser.c
+++ source/parser/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlamark- <dlamark-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 20:07:44 by dlamark-          #+#    #+#             */
-/*   Updated: 2024/04/10 20:08:21 by dlamark-         ###   ########.fr       */
+/*   Updated: 2024/05/07 14:59:53 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int	parsing(t_list *lst_tokens, t_list	*lst_env, char *input)
 {
-	if (syntax_error(lst_tokens, lst_env, input))
+	lst_tokens = type_assignment(lst_tokens);
+	if(!is_closed(input) ||syntax_error(lst_tokens, lst_env, input))
 		return (0);
 	lst_tokens = remove_quotes(lst_tokens);
 	return (1);
