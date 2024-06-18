b4d9b547bc5d85cde64bb7e56f49af2dda590495
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2024-06-18 18:22:18 +0000

    mexendo nos heredocs

--- source/parser/heredoc_expansion.c
+++ source/parser/heredoc_expansion.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc_expansion.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlamark- <dlamark-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 23:43:41 by dlamark-          #+#    #+#             */
-/*   Updated: 2024/06/17 21:33:25 by dlamark-         ###   ########.fr       */
+/*   Updated: 2024/06/18 14:07:17 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int		count_var_size(char *input);
 char	*get_env_value(int counter, char *input);
 int		get_input_len(char *input);
+char	*get_str(int *i, int counter, char *input);
 
 char	*return_var(char *input)
 {
@@ -22,36 +23,54 @@ char	*return_var(char *input)
 	int		counter;
 	char	*env_value;
 	char	*input_expanded;
-	int		input_len;
 	char	*str;
 
 	i = 0;
 	counter = 0;
 	env_value = NULL;
 	input_expanded = NULL;
-	input_len = 0;
 	str = NULL;
 	while (input[i] != '\0')
 	{
-		input_len = get_input_len(input);
-		if (input_len > 0)
-		{
-			str = ft_calloc(input_len + 1, sizeof(char));
-			ft_strlcpy(str, &input[i], input_len + 1);
-			if (input[i + input_len - 1])
-				i += input_len;
-		}
+		counter = get_input_len(input);
+		if (counter > 0)
+			str = get_str(&i, counter, input);
 		if (input[i] == '$' && input[i + 1])
-		{
-			counter = count_var_size(&input[i + 1]);
-			env_value = get_env_value(counter, &input[i + 1]);
-			if (input[i + counter])
-				i += counter - 1;
+			env_value = get_env_str(&i, counter, input);
+		if (str && env_value)
 			input_expanded = ft_strjoin(str, env_value);
-		}
 		i++;
 	}
+	if (!str && env_value)
+		input_expanded = env_value;
+	if (str && !env_value)
+		input_expanded = str;
 	return (input_expanded);
+}
+
+char	*get_env_str(int *i, int counter, char *input)
+{
+	char	*env_value;
+
+	env_value = NULL;
+	counter = count_var_size(&input[*i + 1]);
+	env_value = get_env_value(counter, &input[*i + 1]);
+	if (input[*i + counter])
+		*i += counter - 1;
+	return (env_value);
+}
+
+char	*get_str(int *i, int counter, char *input)
+{
+	char	*str;
+
+	(void) input;
+	str = NULL;
+	str = ft_calloc(counter + 1, sizeof(char));
+	ft_strlcpy(str, &input[*i], counter + 1);
+	if (input[*i + counter - 1])
+		*i += counter;
+	return (str);
 }
 
 int	count_var_size(char *input)
