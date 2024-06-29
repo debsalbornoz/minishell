9d4aa702ed298cf564867c1a4e70b2b0db27bd0e
debora <debsalbornoz@gmail.com>
2024-06-29 16:38:05 -0300

    quase laaaa

--- source/builtins/pwd.c
+++ source/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: dlamark- <dlamark-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 20:36:01 by dlamark-          #+#    #+#             */
-/*   Updated: 2024/06/29 16:26:18 by dlamark-         ###   ########.fr       */
+/*   Updated: 2024/06/29 16:28:42 by dlamark-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	mini_pwd(char **exec)
 {
 	char	path[2048];
+
 	(void)exec;
 	//if (ft_str_exist(*(exec + 1)))
 	//	return (printf("pwd: too many args for command\n"), 1);
